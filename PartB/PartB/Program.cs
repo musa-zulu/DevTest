9965a1518a5ed2323c4ac7544eebf99298556c15
musa <Musaz@shandonbiz.com>
2021-07-19 21:04:21 +0200

    added part B

---
+++ PartB/PartB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PartB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = { 10.61m, 7.92m, 6.98m, 9.14m, 3.13m };
+            numbers = numbers.Reverse().ToArray();
+
+            foreach (var element in numbers)
+                Console.Write(element + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
